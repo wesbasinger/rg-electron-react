0269857dab8f05ed675eea7d9303800fb35541ed
Wes Basinger <dbumathlete@gmail.com>
2020-10-24 20:23:51 -0500

    Looping through work centers and setting state on the dropdown.

--- src/components/TransitionScreen.js
+++ src/components/TransitionScreen.js
@@ -5,10 +5,45 @@ import RemainingWorkCenters from './RemainingWorkCenters';
 
 class TransitionScreen extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      wc: "",
+      setupQty: "",
+      prodQty: ""
+    }
+
+    this.handleWcChange = this.handleWcChange.bind(this);
+    //this.handleSetupQtyChange = this.handleSetupQtyChange.bind(this);
+    //this.handleProdQtyChange = this.coatHandleSecPerBoard.bind(this);
+  }
+
+  handleWcChange(e) {
+    this.setState({wc: e.target.value});
+  }
+
   render() {
       return(
         <div>
-          <RemainingWorkCenters workCenters={this.props.values} />
+          <div>
+            <RemainingWorkCenters workCenters={this.props.values} />
+          </div>
+          <div>
+            <form>
+              <label>Work Center</label>
+              <select onChange={this.handleWcChange}>
+                <option value=""></option>
+                {
+                  this.props.values.map((val) => {
+                    return(
+                      <option key={val.desc} value={val.desc}>{val.desc}</option>
+                    )
+                  })
+                }
+              </select>
+            </form>
+          </div>
         </div>
       )
   }
