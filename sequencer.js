f509d4464fb31b6de2ed43b27971be7964a15d92
Wes Basinger <dbumathlete@gmail.com>
2020-11-13 07:18:25 -0600

    Intent is to do sequencing with a separate function.

---
+++ sequencer.js
@@ -0,0 +1,21 @@
+module.exports = (releaseSize, rtgSteps) => {
+  let currSeq = 10
+  const result = []
+
+  
+}
+
+/*
+releaseSize={this.state.values.releaseSize} rtg={this.state.rtg}
+assemblyNumber={this.state.values.assemblyNumber}
+*/
+
+/*
+<tr key={idx}>
+  <td>{(idx+1)*10}</td>
+  <td>{op.desc}</td>
+  <td>{Math.round(op.setupTime * 1000) / 1000}</td>
+  <td>{Math.round(op.prodTime * 1000) / 1000}</td>
+  <td>{Math.round(op.setupTime + op.prodTime*this.props.releaseSize * 1000) / 1000}</td>
+  <td>{op.note}</td>
+  */
